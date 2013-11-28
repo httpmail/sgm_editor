e9be23316a7a0c8c507fb674aadd8cb558839817
james <james.jiang@nokia.com>
2013-11-29 00:57:04 +0800

    CSGMMaskEdit onCopy onCut onPaste functions

--- MFCApplication9/MFCApplication9/SGMMaskEdit.h
+++ MFCApplication9/MFCApplication9/SGMMaskEdit.h
@@ -13,13 +13,13 @@ public:
 
 public:
     void SetValidString(LPCTSTR lpszValid, BOOL bNoCase = TRUE);
-
 protected:
     BOOL NeedCheckLegalCharacters() const;
     BOOL IsLegalCharacters(WCHAR wChar) const;
     BOOL IsLegalCharacters(const CString &srcString) const;
     BOOL IsInRange(const CString &refString, const CString &srcString) const;
-
+    BOOL CopyEditDataToClipboard();
+    BOOL ExtractDataFromClipboard(CString& srcString);
 private:
     CString m_MaskStr;
     BOOL    m_bNoCase;
@@ -30,7 +30,13 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
     afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
-    afx_msg void OnEnMaxtext();
+
+    afx_msg void OnCut(void);
+    afx_msg void OnCopy(void);
+    afx_msg void OnPaste(void);
+    afx_msg void OnUndo(void);
+
+    afx_msg void OnEnMaxtext(void);
 };
 
 
